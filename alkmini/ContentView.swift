644c0a389c130962b62e54426661239179db073e
Alberto Morgante Medina <alknopfler@gmail.com>
2023-06-09 21:17:50 +0200

    Initial Commit

---
+++ alkmini/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  alkmini
+//
+//  Created by Alberto Morgante Medina on 9/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ alkmini/alkminiApp.swift
@@ -0,0 +1,17 @@
+//
+//  alkminiApp.swift
+//  alkmini
+//
+//  Created by Alberto Morgante Medina on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct alkminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
